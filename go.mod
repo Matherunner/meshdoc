c55efcc5525d31d4f88caac47ae797c995259ff7
Chong Jiang Wei <randolphchong@gmail.com>
2022-03-05 21:27:45 +0800

    Add writer handlers

--- go.mod
+++ go.mod
@@ -2,7 +2,9 @@ module github.com/Matherunner/meshdoc
 
 go 1.16
 
+replace github.com/Matherunner/meshforce => /home/cjw/go/src/github.com/Matherunner/meshforce
+
 require (
 	github.com/BurntSushi/toml v1.0.0
-	github.com/Matherunner/meshforce v0.0.0-20220305092136-b03b4aeb0722
+	github.com/Matherunner/meshforce v0.0.0-20220305105948-8555c0ce7385
 )
--- meshdoc.go
+++ meshdoc.go
@@ -1,13 +1,94 @@
 package meshdoc
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"path"
 
 	"github.com/BurntSushi/toml"
 	"github.com/Matherunner/meshforce"
+	"github.com/Matherunner/meshforce/tree"
+	"github.com/Matherunner/meshforce/writer/html"
 )
+
+type lineScanner struct {
+	scanner *bufio.Scanner
+	i       int
+	eof     bool
+	peekBuf string
+	peeked  bool
+}
+
+func newLineScanner(scanner *bufio.Scanner) *lineScanner {
+	eof := !scanner.Scan()
+	return &lineScanner{scanner: scanner, eof: eof}
+}
+
+func (s *lineScanner) Scan() bool {
+	s.i++
+	s.eof = !s.scanner.Scan()
+	s.peeked = false
+	return !s.eof
+}
+
+func (s *lineScanner) Line() string {
+	if s.peeked {
+		return s.peekBuf
+	}
+	return s.scanner.Text()
+}
+
+func (s *lineScanner) LineNumber() int {
+	return s.i
+}
+
+func (s *lineScanner) EOF() bool {
+	return s.eof
+}
+
+func (s *lineScanner) Peek() bool {
+	if s.eof {
+		return false
+	}
+	s.peekBuf = s.scanner.Text()
+	s.peeked = true
+	return true
+}
+
+type nodeDefinitions struct {
+}
+
+func newNodeDefinitions() *nodeDefinitions {
+	return &nodeDefinitions{}
+}
+
+func (d *nodeDefinitions) Register(p *meshforce.Parser) {
+	for _, def := range meshforce.BlockDefinitions {
+		p.RegisterBlock(def)
+	}
+	for _, def := range meshforce.InlineDefinitions {
+		p.RegisterInline(def)
+	}
+}
+
+type treeVisitor struct {
+}
+
+func newTreeVisitor() *treeVisitor {
+	return &treeVisitor{}
+}
+
+func (v *treeVisitor) Enter(cur *tree.Node, stack []*tree.Node) (instruction tree.VisitInstruction, err error) {
+	return
+}
+
+func (v *treeVisitor) Exit(cur *tree.Node, stack []*tree.Node) (err error) {
+	return
+}
 
 type meshdocConfig struct {
 	Path string
@@ -40,12 +121,55 @@ func (m *Meshdoc) Run() (err error) {
 		return
 	}
 
-	for _, file := range fileInfo {
-		filePath := path.Join(m.config.Path, file.Name())
-		log.Printf("path = %+v\n", filePath)
+	treeByPath := map[string]*tree.Tree{}
 
+	definitions := newNodeDefinitions()
+
+	writer := html.NewWriter()
+	writer.RegisterBlockHandler(&TitleHandler{})
+	writer.RegisterBlockHandler(&H1Handler{})
+	writer.RegisterBlockHandler(&ParagraphHandler{})
+	writer.RegisterInlineHandler(&StrongHandler{})
+	writer.RegisterInlineHandler(&EmphasisHandler{})
+
+	for _, info := range fileInfo {
+		filePath := path.Join(m.config.Path, info.Name())
+		log.Printf("Parsing %s\n", filePath)
+
+		var file *os.File
+		file, err = os.Open(filePath)
+		if err != nil {
+			return
+		}
+
+		scanner := bufio.NewScanner(file)
+		peeker := newLineScanner(scanner)
 		parser := meshforce.NewParser()
-		log.Printf("parser = %+v\n", parser)
+
+		definitions.Register(parser)
+
+		parser.Parse(peeker)
+
+		msgs := parser.Messages().Messages()
+		for _, msg := range msgs {
+			if msg.Kind == meshforce.MessageKindError {
+				return fmt.Errorf("parse error: %+v", msg)
+			}
+		}
+
+		treeByPath[filePath] = parser.Tree()
+
+		buf := bytes.Buffer{}
+		err = writer.Write2(&buf, parser.Tree())
+		if err != nil {
+			return
+		}
+
+		content := buf.String()
+		log.Printf("content = %+v\n", content)
+
+		// visitor := newTreeVisitor()
+		// parser.Tree().Visit(visitor)
 	}
 
 	return
